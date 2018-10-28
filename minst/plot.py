80f470768f98b075068eb42838631d34abb417ec
kahofan <zyclionel@outlook.com>
2018-10-28 10:38:35 +0800

    Update minst/plot.py

---
+++ minst/plot.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 26 13:41:42 2018
+
+@author: Lionel
+"""
+
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot = False)
+x_train = mnist.train._images
+y_train = mnist.train._labels
+
+fig, ax = plt.subplots(
+        nrows = 2,
+        ncols = 5,
+        sharex = True,
+        sharey = True)
+ax = ax.flatten()
+for i in range(10):
+    img = x_train[y_train == i][0].reshape(28, 28)
+    ax[i].imshow(img, cmap = 'Greys', interpolation = 'nearest')
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
